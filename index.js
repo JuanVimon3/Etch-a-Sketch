516852372c3e3ee85aa4ccb9e3a95c9b6ae103bf
JuanVimon3 <jvillamonsalve@gmail.com>
2024-06-21 12:00:05 -0500

    wip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let container = document.getElementById('container');
     let rows = 16;
     let columns = 16;
+    
     for(let i = 0; i < rows; i++){
         for(let j = 0; j < columns; j++){
             let item = document.createElement('div');
@@ -15,3 +16,33 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+function restart () {
+    const newSize = window.prompt("Please input a new size between 1 and 100:")
+    const newSizeInt = parseInt(newSize);
+    console.log(newSizeInt);
+    const container = document.getElementById("container");
+    container.innerHTML = '';
+    const newRows = newSize;
+    const newColumns = newSize;
+
+    if (newSize <= 100){
+        for(let i = 0; i < newRows; i++){
+            for(let j = 0; j < newColumns; j++){
+                let newItem = document.createElement('div');
+                newItem.classList.add('newItem')
+    
+                newItem.addEventListener('mouseover', function(){
+                    newItem.classList.add('hovered')
+                })
+    
+                container.appendChild(newItem)
+            }
+        } 
+    }else{
+        restart()
+    }
+}
+
+// borrar el contenido de los hijos de container 
+//crear nueva funciòn con la lògica del nuevo grid redimensionado
